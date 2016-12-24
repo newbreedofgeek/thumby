60dadaf2077cec128ddad71926ce0c62e2d68874
newbreedofgeek <paul.mark@gmail.com>
2016-12-24 18:17:17 +1100

    WIP - fixing lwip install error on node 6.9

--- src/config.js
+++ src/config.js
@@ -11,4 +11,12 @@ export default {
 };
 
 // **
-// By default thumby stores images in the local "/public/imgs" folder. but this only works if you are running thumby in a standalone server. if you are running it in a container (like docker or dokku) then "/public/imgs" gets nuked at each rebuild. in this case you should mount a persistent host directory to a virtual directory called "/storage" inside your docker container. for e.g. you can mount "/var/lib/your-app/storage" in your host server to "/storage" inside your docker container and then change the "storageRoot" config setting in "/src/config.js" to be '/storage'. the directory names "storage" is recommended as in the code found in /app.js we set this value as a static location so you can reference your images via the web.
+// By default thumby stores images in the local "/public/imgs" folder.
+// but this only works if you are running thumby in a standalone server.
+// if you are running it in a container (like docker or dokku) then "/public/imgs" gets
+// nuked at each rebuild. in this case you should mount a persistent host directory to a
+// virtual directory called "/storage" inside your docker container.
+// for e.g. you can mount "/var/lib/your-app/storage" in your host server to "/storage"
+// inside your docker container and then change the "storageRoot" config setting in "/src/config.js"
+// to be '/storage'. the directory names "storage" is recommended as in the code found in /app.js
+// we set this value as a static location so you can reference your images via the web.
